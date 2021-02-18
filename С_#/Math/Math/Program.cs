8d92c1bf6cacbf7c0ec1ff8a33d6851baef4fa03
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2021-02-18 23:58:36 +0300

    added M

--- С_#/Math/Math/Program.cs
+++ С_#/Math/Math/Program.cs
@@ -5,8 +5,10 @@ class B { }
 class C { }
 class Program
 {
+    
     static void Main()
     {
+        int Mp = 0;
         Console.WriteLine("Параметр а:");
         int A = Convert.ToInt32(Console.ReadLine()); 
         Console.WriteLine("Параметр b:");
@@ -14,7 +16,10 @@ class Program
         Console.WriteLine("Параметр c:");
         int C = Convert.ToInt32(Console.ReadLine());
         double result = Math.Sqrt(Math.Pow(A,2) + Math.Pow(B,2) - (2 * A * B*Math.Cos(C)));
-        Console.WriteLine(result);
+        Console.WriteLine($"Результат выполнения первой задачи - {result}");
+        if (A > 1 || A % A == 0 || A % 1 == A)
+            Mp = (2 * A) - 1;
+            Console.WriteLine($"Число Мерсенна ==> { Mp}");
     }
 }
 
