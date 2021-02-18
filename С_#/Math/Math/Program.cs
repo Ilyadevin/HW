8f89290e1790326e83b9438675a9f5901de24f39
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2021-02-18 21:45:20 +0300

    added MATH

---
+++ С_#/Math/Math/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Параметр а - ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Параметр b - ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Параметр c - ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        double result = Math.Sqrt((a * a) + (b * b) - (2 * a * b*Math.Cos(c)));
+        Console.WriteLine(result);
+    } 
+}
+
